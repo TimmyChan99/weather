442372926087dd94ecb052aad3ab2885e39671e7
Fatima Ezzahra <felemenoun@gmail.com>
2022-01-27 00:37:11 +0100

    Update script >> Add clock

--- src/index.js
+++ src/index.js
@@ -48,3 +48,26 @@ getLocation.addEventListener('submit', (e) => {
     getCurrentWeather(locationInput.value);
     getLocation.reset();
 });
+
+// Display Time
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
+ 
+    const time = document.querySelector('.time');
+
+    function startTime() {
+        const today = new Date();
+        let h = today.getHours();
+        let m = today.getMinutes();
+        let s = today.getSeconds();
+        m = checkTime(m);
+        s = checkTime(s);
+        time.innerHTML =  h + ":" + m + ":" + s;
+        setTimeout(startTime, 1000);
+      }
+      
+startTime();
